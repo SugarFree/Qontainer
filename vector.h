e1cd94b784e2a842fc589605748e1b7431278e19
SugarFree <vittoriocorrizzato@gmail.com>
2018-12-12 16:24:25 +0100

    Completamento dichiarazione vector

--- vector.h
+++ vector.h
@@ -1,12 +1,21 @@
 #ifndef VECTOR_H
 #define VECTOR_H
+#include<iostream>
+
+template<class T>
+class vector;
+
+template<class T>
+std::ostream& operator<<(std::ostream&, const vector<T>&);
 
 template<class T>
 class vector {
+friend std::ostream& operator<< <T>(std::ostream&, const vector<T>&);
 private:
+    unsigned int capacity;
     unsigned int size;
     T* point;
-    int* copy() const;
+    T* copy() const;
 public:
     vector(unsigned int =0, T* =nullptr);
     vector(const vector<T>&);
@@ -15,20 +24,27 @@ public:
     void push_back(const T&);
     void pop_back();
     bool empty() const;
-    // void clear(); per pulire tutta una config. Basta distruttore???
+    void clear();
+    unsigned int getCapacity() const;
     unsigned int getSize() const;
-    void resize(unsigned int size);
+    void resize(unsigned int);
     T& operator[](unsigned int);
     class constiterator {
     friend class vector<T>;
     private:
         const T* pt;
-
+        bool eov;
+        constiterator(T* =nullptr, bool =false);
+    public:
+        constiterator();
+        constiterator& operator++();
+        constiterator& operator--();
+        const T& operator*() const;
+        bool operator==(const constiterator&) const;
+        bool operator!=(const constiterator&) const;
     };
+    constiterator begin() const;
+    constiterator end() const;
 };
-
-
-
-std::ostream& vector<T> operator<<(std::ostream& os, const vector<T>& v);
 
 #endif // VECTOR_H
