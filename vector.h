7d9a252b531e634b483dee4cb3919f858df610a2
vcorrizz <vcorrizz@t35.torre.math.unipd.it>
2018-12-12 11:19:11 +0100

    Modifiche container vector

--- vector.h
+++ vector.h
@@ -5,18 +5,29 @@ template<class T>
 class vector {
 private:
     unsigned int size;
-    T* punt;
+    T* point;
+    int* copy() const;
 public:
-    vector(unsigned int =0, unsigned int =0, T* =nullptr);
+    vector(unsigned int =0, T* =nullptr);
     vector(const vector<T>&);
     vector<T>& operator=(const vector<T>&);
     ~vector();
     void push_back(const T&);
     void pop_back();
+    bool empty() const;
+    // void clear(); per pulire tutta una config. Basta distruttore???
+    unsigned int getSize() const;
     void resize(unsigned int size);
     T& operator[](unsigned int);
+    class constiterator {
+    friend class vector<T>;
+    private:
+        const T* pt;
 
+    };
 };
+
+
 
 std::ostream& vector<T> operator<<(std::ostream& os, const vector<T>& v);
 
